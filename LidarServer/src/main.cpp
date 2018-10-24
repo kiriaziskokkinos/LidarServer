d462d020324956ee964f711dafb42fbbb577f122
Thanasis Liatifis <thanasisslt@gmail.com>
2018-10-24 19:45:14 +0300

    Included Eigen libraray in project

--- LidarServer/src/main.cpp
+++ LidarServer/src/main.cpp
@@ -15,11 +15,21 @@
 #include "ConnectionManager.h"
 #include "Logger.h"
 #include <thread>
+#include "../Eigen/Core"
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    
+    //testing Eigen included library
+    Eigen::MatrixXd m(2,2);
+
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
+    //end of test
+
     ConnectionManager connMan;
     Logger::initLogger();
     Logger::addLog("~~~~~ STARTING SERVER ~~~~~");
